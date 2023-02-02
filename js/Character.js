72edb6ddc77a762834b73d876d669283f6036fcc
Zile10 <johnnoad04@gmail.com>
2023-02-02 16:53:18 +0200

    update'

--- js/Character.js
+++ js/Character.js
@@ -1,29 +1,45 @@
+const gravity = 0.98
 class Character {
   constructor(config) {
     this.name = config.name
-    this.position = config.postion
+    this.position = config.position
     this.speed = config.speed || 5
     this.jumpSpeed = config.jumpSpeed || 20
+    this.dimensions = config.dimensions
 
     this.image = new Image();
     this.image.src = config.imageSrc;
   }
 
-  moveLeft(){
-    this.position.x -= this.speed
-  }
-  jump(){
-    this.position.y += this.jumpSpeed
-  }
-  moveRight(){
-    this.position.x += this.speed
-  }
-
   render(context) {
     context.drawImage(this.image, this.position.x, this.position.y);
   }
+  draw(ctx){
+    ctx.fillStyle = 'crimson';
+    ctx.fillRect(
+      this.position.x, 
+      this.position.y, 
+      this.dimensions.width, 
+      this.dimensions.height
+    );
+  }
+
+  update(){
+    this.draw()
+    if (this.x + this.radius + this.dx > canvas.width || this.x - this.radius + this.dx < 0) {
+        this.dx = -this.dx  //* 0.9
+    }
+    if (this.position.y + this.dimensions.height + this.dy > canvas.height || this.position.y - this.dimensions.height + this.dy < 0) {
+        this.dy = -this.dy //* 0.9
+        this.dx = this.dx  //* 0.98
+    } else {
+        this.dy += gravity
+    }
+    this.x += this.dx
+    this.y += this.dy
+  }
 }
 
-class Player extends Character {
+// class Player extends Character {
 
-}
+// }
--- js/init.js
+++ js/init.js
@@ -4,37 +4,79 @@ const ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-
-// Instantiations:
-
-let overworld = new Map({
-  bakcground: '',
-  middleground: '',
-  platform: '',
-  foreground: '',
-})
-
-let player = new Character({
-  name: 'INTJ'
-})
-
-// Event Listeners:
-
-document.addEventListener('keydown', event => {
-  switch(event.key.toLowerCase()) {
-    case 'a':
-      player.moveLeft();
-      break;
-    case 'w':
-      player.jump();
-      break;
-    case 'd':
-      player.moveRight();
-      break;
-  }
+const player = new Character({
+  position: {
+    x: 300,
+    y: 300,
+  },
+  dimensions: {
+    width: 50,
+    height: 100
+  },
+  speed: 5
 });
 
+// let player = new Character()
 
+const keys = {
+  w: {
+      pressed: false,
+  },
+  a: {
+      pressed: false,
+  },
+  s: {
+      pressed: false,
+  },
+  d: {
+      pressed: false,
+  },
+}
+
+let lastKey = ''
+addEventListener('keydown', (e) => {
+    switch (e.key.toLowerCase()) {
+        case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
+            break;
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break;
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+            break;
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break;
+    
+        default:
+            break;
+    }
+})
+
+addEventListener('keyup', (e) => {
+    switch (e.key.toLowerCase()) {
+        case 'w':
+            keys.w.pressed = false
+            break;
+        case 'a':
+            keys.a.pressed = false
+            break;
+        case 's':
+            keys.s.pressed = false
+            break;
+        case 'd':
+            keys.d.pressed = false
+            break;
+    
+        default:
+            break;
+    }
+})
 
 // Functions:
 function setCanvasDimensions() {
@@ -46,7 +88,19 @@ function animate(){
   requestAnimationFrame(animate)
   setCanvasDimensions()
   ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas before render
-  player.render(ctx);
+  // player.render(ctx);
+  player.draw()
+
+  if (keys.w.pressed && lastKey === 'w') {
+    // player.position.y += player.speed
+  } else if (keys.a.pressed && lastKey === 'a') {
+      player.position.x -= player.speed
+  } else if (keys.s.pressed && lastKey === 's') {
+      // player.position.y -= player.speed
+  } else if (keys.d.pressed && lastKey === 'd') {
+      player.position.x += player.speed
+  }
+
 }
 
 animate()
